9564a25b16695d33d38b02c6dd7abe5f6605e229
tujiawei <you@example.com>
2022-01-29 14:13:02 +0800

    feat(all):自动提交

--- packages/bin/src/shell/init.sh
+++ packages/bin/src/shell/init.sh
@@ -36,14 +36,20 @@ function VimInstall() {
   # 下载依赖，如果有了再安装一遍也没问题
   apt install git make clang libtool-bin -y
 
-  # 支持gui,需要支持一下自提
-  apt install libgtk-3-dev
+  # 支持剪切板，并且这是gui的依赖项
+  apt install libxt-dev
+
+  # 支持gui,需要支持一下字体
+  apt install libgtk-3-dev -y
 
   # 支持 python
-  apt install libpython3-dev
+  # 在Makefil里 把下面这行的注释去掉
+  # CONF_OPT_PYTHON3 = --enable-python3interp
+  apt install libpython3-dev -y
 
   GitDownload https://github.com/vim/vim.git --depth 1
   cd vim/src
+  make reconfig
   make
   make install
   cd -
