bb0e0da866c5c25479cf58dc0a9cbc3e46e120e8
tujiawei <you@example.com>
2022-06-21 10:51:57 +0800

    feat(utils): add deepEqual
    添加深层比较

--- packages/utils/src/utils/utils.ts
+++ packages/utils/src/utils/utils.ts
@@ -777,3 +777,38 @@ export function C(m: number, n: number) {
   }
   return result;
 }
+
+/**
+ * 深层比较两个值是否相等
+ */
+export function deepEqual(a: unknown, b: unknown): boolean {
+  if (isSame(a, b)) {
+    return true;
+  }
+
+  if (
+    typeof a != 'object' ||
+    a === null ||
+    typeof b != 'object' ||
+    b === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(a as object);
+  const keysB = Object.keys(b as object);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  return keysA.every((key) => {
+    return (
+      Object.hasOwnProperty.call(b, key) &&
+      deepEqual(
+        (a as Record<string, unknown>)[key],
+        (b as Record<string, unknown>)[key]
+      )
+    );
+  });
+}
