677257372643c2f25375b15e9613ab25c5120151
tujiawei <you@example.com>
2022-06-25 19:55:22 +0800

    feat(List): 链表

---
+++ packages/utils/src/data-structure/List/LinkedList/LinkedNode.ts
@@ -0,0 +1,44 @@
+/**
+ * 单向链表节点
+ * @filename: /home/tujiawei/github/package/packages/utils/src/data-structure/List/LinkedList/LinkedListNode.ts
+ * @author: Mr Prince
+ * @date: 2022-06-24 21:08:25
+ */
+class LinkedListNode<T> {
+  /**
+   * 节点值
+   */
+  private value: T;
+
+  /**
+   * 下一个节点
+   */
+  private next: LinkedListNode<T>;
+
+  constructor(value: T, next?: LinkedListNode<T>) {
+    this.value = value;
+    this.next = undefined === next ? null : next;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(value: T) {
+    this.value = value;
+  }
+
+  hasNext() {
+    return null != this.next;
+  }
+
+  getNext() {
+    return this.next;
+  }
+
+  setNext(next: LinkedListNode<T>) {
+    this.next = next;
+  }
+}
+
+export default LinkedListNode;
