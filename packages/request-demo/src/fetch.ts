286a8737f684d1c8632c22f0a11592edba81754d
tujiawei <jiaweitu@marcothepolo.com>
2024-03-14 11:01:26 +0800

    feat(fetch): add type support and fetch

--- packages/request-demo/src/fetch.ts
+++ packages/request-demo/src/fetch.ts
@@ -1,5 +1,6 @@
-const fetchRequest = () => {
-  fetch('https://www.demo.com');
+const fetchRequest = async (url: string, init?: RequestInit) => {
+  const response = await fetch(url, init);
+  return response;
 };
 
 export default fetchRequest;
