c93046e1a801e093e576d2da26608933d54bd68b
tujiawei <jiaweitu@marcothepolo.com>
2024-05-09 10:52:07 +0800

    fix

--- packages/demo-redis/src/index.ts
+++ packages/demo-redis/src/index.ts
@@ -31,8 +31,8 @@
 //
 // main();
 
-import log4js from 'log4js';
+// import log4js from 'log4js';
 
-const logger = log4js.getLogger();
-logger.level = 'debug';
-logger.debug('Some debug messages');
+// const logger = log4js.getLogger();
+// logger.level = 'debug';
+// logger.debug('Some debug messages');
