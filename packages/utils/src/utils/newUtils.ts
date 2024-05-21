294be4ca8ed46c3574866ebf4861c2aa30f11027
tujiawei <jiaweitu@marcothepolo.com>
2024-05-21 16:56:55 +0800

    feat(newUtils): add weightedRandom

--- packages/utils/src/utils/newUtils.ts
+++ packages/utils/src/utils/newUtils.ts
@@ -88,3 +88,33 @@ export const validateCreditCard = (creditCard: string) => {
 
   return true;
 };
+
+/**
+ * 加权随机
+ */
+export const weightedRandom = (items: number[], weights: number[]) => {
+  if (items.length !== weights.length) {
+    throw new Error('Items and weights must be of the same size');
+  }
+
+  if (!items.length) {
+    throw new Error('Items must not be empty');
+  }
+
+  const cumulativeWeights: number[] = new Array(items.length);
+  for (let i = 0; i < weights.length; i += 1) {
+    cumulativeWeights[i] = weights[i] + (cumulativeWeights[i - 1] || 0);
+  }
+
+  const maxCumulativeWeight = cumulativeWeights[cumulativeWeights.length - 1];
+  const randomNumber = maxCumulativeWeight * Math.random();
+
+  for (let itemIndex = 0; itemIndex < items.length; itemIndex += 1) {
+    if (cumulativeWeights[itemIndex] >= randomNumber) {
+      return {
+        item: items[itemIndex],
+        index: itemIndex,
+      };
+    }
+  }
+};
