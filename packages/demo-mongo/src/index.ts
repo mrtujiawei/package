8f10bd640ac2db0baac6ffd6adc31886580f60e7
tujiawei <jiaweitu@marcothepolo.com>
2024-06-19 10:49:39 +0800

    feat(demo-mongo): add demo of mongo package

---
+++ packages/demo-mongo/src/index.ts
@@ -0,0 +1,14 @@
+/**
+ *
+ * @filename src/index.ts
+ * @author  tujiawei <jiaweitu@marchthepolo.com>
+ * @date 2024-06-05 19:33:34
+ */
+
+class User {
+  public static readonly name = 'tujiawei';
+
+  public static async main(args: string[]) {
+    console.log(args);
+  }
+}
