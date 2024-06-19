178e344b9921c2fdac71ddc72809648ba6c16bba
tujiawei <jiaweitu@marcothepolo.com>
2024-06-19 13:43:45 +0800

    feat(MapEnhancer): add MapEnhancer utils

--- packages/utils/src/data-structure/MapEnhancer/MapEnhancer.ts
+++ packages/utils/src/data-structure/MapEnhancer/MapEnhancer.ts
@@ -50,6 +50,60 @@ class MapEnhancer {
 
     return defaultValue;
   }
+
+  /**
+   * 如果没有 key 时设置 value
+   *
+   * @returns 如果已存在，返回之前的值；否则返回新值
+   */
+  static setIfAbsent<K, V>(map: Map<K, V>, key: K, value: V) {
+    const prevValue = map.get(key)!;
+    if (map.has(key)) {
+      return prevValue;
+    }
+    map.set(key, value);
+    return prevValue;
+  }
+
+  /**
+   * 如果有 set 过 key, 则 set 为新 value
+   */
+  static setIfPresent<K, V>(map: Map<K, V>, key: K, value: V) {
+    const prevValue = map.get(key)!;
+    if (map.has(key)) {
+      map.set(key, value);
+    }
+    return prevValue;
+  }
+
+  /**
+   * @returns map.size == 0
+   */
+  static isEmpty(map: Map<unknown, unknown>) {
+    return map.size == 0;
+  }
+
+  /**
+   * set all 如果已存在 key, value 会被 entries 中的覆盖
+   */
+  static setAll<K, V>(map: Map<K, V>, entries: Map<K, V>) {
+    entries.forEach((value, key) => {
+      map.set(key, value);
+    });
+  }
+
+  /**
+   * 替换指定 key 的 value
+   *
+   * @returns true if success, else false
+   */
+  static replace<K, V>(map: Map<K, V>, key: K, oldValue: V, newValue: V) {
+    if (map.has(key) && map.get(key) == oldValue) {
+      map.set(key, newValue);
+      return true;
+    }
+    return false;
+  }
 }
 
 export default MapEnhancer;
