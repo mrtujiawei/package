c983cc32f45d8403bd2f23a2fa1984966d21f14c
tujiawei <jiaweitu@marcothepolo.com>
2024-07-01 17:50:03 +0800

    refactor(Comparator): add method

--- packages/utils/src/utils/Comparator.ts
+++ packages/utils/src/utils/Comparator.ts
@@ -1,4 +1,10 @@
 class Comparator<T> {
+  private static readonly MINUS_ONE = -1;
+
+  private static readonly ZERO = 0;
+
+  private static readonly ONE = 1;
+
   /**
    * @param compare - 比较函数
    */
@@ -8,21 +14,28 @@ class Comparator<T> {
    * 判断是否相等
    */
   equal(a: T, b: T) {
-    return this.compare(a, b) === 0;
+    return this.compare(a, b) === Comparator.ZERO;
+  }
+
+  /**
+   * 判断是否不想等
+   */
+  notEqual(a: T, b: T) {
+    return !this.equal(a, b);
   }
 
   /**
    * a < b ? true : false
    */
   lessThan(a: T, b: T) {
-    return this.compare(a, b) < 0;
+    return this.compare(a, b) < Comparator.ZERO;
   }
 
   /**
    * a > b ? true : false
    */
   greaterThan(a: T, b: T) {
-    return this.compare(a, b) > 0;
+    return this.compare(a, b) > Comparator.ZERO;
   }
 
   /**
@@ -37,6 +50,27 @@ class Comparator<T> {
    */
   greaterThanOrEqual(a: T, b: T) {
     return this.greaterThan(a, b) || this.equal(a, b);
+  }
+
+  /**
+   * value == 0 ? true | false
+   */
+  isZero(value: T) {
+    return value == Comparator.ZERO;
+  }
+
+  /**
+   * value == 1 ? true : false
+   */
+  isOne(value: T) {
+    return value == Comparator.ONE;
+  }
+
+  /**
+   * value == -1 ? true : false
+   */
+  isMinusOne(value: T) {
+    return value == Comparator.MINUS_ONE;
   }
 }
 
